b3d07eb815ba4ab7f426a569237eeabc88dfd39e
solrun2 <solrun555@gmail.com>
2022-08-03 01:32:25 +0700

    solrun

---
+++ 07PB/08.py
@@ -0,0 +1,23 @@
+import math
+
+def isSquare(n) :
+    root = math.sqrt(n)
+    if n == 0 or n == 1 :
+        return False
+    elif int(root)**2 == n :
+        return True
+    else :
+        return False
+
+n = int(input())
+cnt = 0
+check = n**2 // 2
+i = check
+while i > 1 :
+    if not isSquare(i) :
+        i -= 1
+        continue
+    if isSquare(int(n**2 - i)) :
+        cnt += 1
+    i -= 1
+print(cnt)
