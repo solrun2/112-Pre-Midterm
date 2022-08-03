0ba0a3056ca52e052d3a3de585c064034ef77420
solrun2 <solrun555@gmail.com>
2022-08-03 21:38:46 +0700

    solrun

--- 05PB/06.py
+++ 05PB/06.py
@@ -1,13 +1,11 @@
 sum = 0
 n = int(input())
-i = 1
 while True :
     if n == 0 :
         break
     d = n%10
     sum += d
     n = n//10
-    i += 1
 if sum % 9 == 0 :
     print("Yes {}".format(sum))
 else :
