18ccbb8309d621bf764a55c43d80ceab76562923
solrun2 <solrun555@gmail.com>
2022-08-25 22:22:03 +0700

    solrun

--- 07PB/03.py
+++ 07PB/03.py
@@ -5,17 +5,17 @@ else :
     i = 1
     c = ord("A")
     while i <= n :
-      j = 1
-      while j <= i :
-        print(chr(c+j-1),end="")
+      j = 0
+      while j < i :
+        print(chr(c+j),end="")
         j += 1
       print("")
       i += 1
     i = n-1
-    while i >= 1 :
-      j = 1
-      while j <= i :
-        print(chr(c+j-1),end="")
+    while i > 0 :
+      j = 0
+      while j < i :
+        print(chr(c+j),end="")
         j += 1
       print("")
       i -= 1
