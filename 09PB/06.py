333ac1213842d9163f3414b9016ad2cda65caede
SoLRuN <77611650+solrun2@users.noreply.github.com>
2022-08-29 14:33:28 +0700

    Delete 09PB directory

--- 09PB/06.py
+++
@@ -1,8 +0,0 @@
-def alternatingSum(n) :
-    if n%2 == 0 :
-        return -n // 2
-    else :
-        return (n+1) // 2
-
-n = int(input("Enter n of series: "))
-print("Alternating Sum from 1 to {:d} is {:d}".format(n,alternatingSum(n)))
